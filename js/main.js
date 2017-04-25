064365b82ce52cedfae50d8988260c204ed29357
Александр Автухович <mr_work89@mail.ru>
2017-04-25 10:24:17 +0200

    js

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Пользователь on 24.04.2017.
+ */
+$(document).ready(function(){
+    $('.bxslider').bxSlider({
+        auto: true,
+        infiniteLoop: false,
+        hideControlOnEnd: true
+    });
+});
